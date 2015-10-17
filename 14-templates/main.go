d9d3f498115c1a9a05b7ed8d8335117264dc36ef
Daniel Hoffmann <danielgamer@sbcglobal.net>
2015-10-17 06:59:20 -0700

    Added some comments

--- 14-templates/main.go
+++ 14-templates/main.go
@@ -5,9 +5,15 @@ import (
 	"net/http"
 )
 
+// Hold your template data globally, it makes it easier.
 var tpl *template.Template
 
 func init() {
+	// During startup, you will need to read in all the template files.
+	// This will send back a pointer to the templates, and an error.
+	// The Must function takes both of those, and if there is an error,
+	// prints an error message and quits. If there was no error,
+	// it will simply return just the template.
 	tpl = template.Must(template.ParseGlob("*.gohtml"))
 
 	http.HandleFunc("/", index)
@@ -16,14 +22,19 @@ func init() {
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
+	// The three arguments is, a Writer that the template will be sent to,
+	// the name of the template to use, and the data to put into the template.
+	// ResponseWriter is smart enough to recognize a full html page and will set Content-Type by itself.
 	tpl.ExecuteTemplate(res, "index", nil)
 }
 
 func foo(res http.ResponseWriter, req *http.Request) {
+	// Here we are sending some data into the template.
 	tpl.ExecuteTemplate(res, "foo", 42)
 }
 
 func bar(res http.ResponseWriter, req *http.Request) {
+	// Here we are creating an anonymous struct type and variable and filling it.
 	model := struct {
 		Name       string
 		HasAccount bool
@@ -31,5 +42,7 @@ func bar(res http.ResponseWriter, req *http.Request) {
 		"Daniel",
 		false,
 	}
-	tpl.ExecuteTemplate(res, "bar", model)
+	// Here we pass the struct to the template.
+	// Note that sending a pointer in more efficient than the value for structs.
+	tpl.ExecuteTemplate(res, "bar", &model)
 }
